cd2ff9d526e6f14c9fe8d40a57dbca9421824b16
Евгений <125446285+EvgenyEvdakov@users.noreply.github.com>
2023-05-10 11:53:57 +0300

    idz3.py

--- idz3.py
+++ idz3.py
@@ -1,4 +1,8 @@
-for i in range(10, 1000):
-    s = i // 10 + i % 10
-    if s + s**2 == i:
-        print(i)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    for i in range(10, 1000):
+        s = i // 10 + i % 10
+        if s + s**2 == i:
+            print(i)
